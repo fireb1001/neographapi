fdb299f434b0dab9ea7b260d09c85be21533de67
Sando Sharsh <fireb1003@gmail.com>
2020-03-25 18:41:11 +0200

    coffebeans

--- src/graphql-schema.js
+++ src/graphql-schema.js
@@ -1,14 +1,17 @@
 import { neo4jgraphql } from "neo4j-graphql-js";
 import fs from "fs";
 import path from "path";
+import dotenv from "dotenv";
 
+// set environment variables from ../.env
+dotenv.config();
 /*
  * Check for GRAPHQL_SCHEMA environment variable to specify schema file
  * fallback to schema.graphql if GRAPHQL_SCHEMA environment variable is not set
  */
 
-export const typeDefs = fs
-  .readFileSync(
-    process.env.GRAPHQL_SCHEMA || path.join(__dirname, "schema.graphql")
-  )
-  .toString("utf-8");
+const file_path = process.env.GRAPHQL_SCHEMA ? 
+  path.join(__dirname, process.env.GRAPHQL_SCHEMA)
+  :  path.join(__dirname, "schema.graphql");
+
+export const typeDefs = fs.readFileSync(file_path).toString("utf-8");
